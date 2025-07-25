7e6c0065ea75f1fe405ceb7cda5ee9ae8e21d9cf
Daniel Gaievskyi <gaievskyi@proton.me>
2025-07-25 19:32:03 +0200

    fix hydration error

--- src/components/sections/footer-content.tsx
+++ src/components/sections/footer-content.tsx
@@ -9,14 +9,14 @@ export function FooterContent({ className }: { className?: string }) {
         className,
       )}
     >
-      <p className="flex items-center gap-2 text-sm">
+      <div className="flex items-center gap-2 text-sm">
         {new Date().getFullYear()},
         <Clock />
-        <div className="inline-flex gap-1">
+        <p className="inline-flex gap-1">
           <span>&copy;</span>
           <span className="hidden lg:inline">Daniel Gaievskyi</span>
-        </div>
-      </p>
+        </p>
+      </div>
       <p className="flex items-center gap-4 text-sm">
         <a href="mailto:daniel@gaievskyi.com" className="hover:text-brand">
           Contact
