35adda7247f11d80191dae4a2c5ac07e0aa7286c
Daniel Gaievskyi <gaievskyi@proton.me>
2026-03-16 11:38:56 +0100

    rm tree

--- src/components/sections/header-content.tsx
+++ src/components/sections/header-content.tsx
@@ -1,4 +1,5 @@
-import { ChristmasTree } from "@/components/christmas-tree"
+// turn on in winter
+// import { ChristmasTree } from "@/components/christmas-tree"
 import { ThemeSwitch } from "@/components/theme-switch"
 import { Flex } from "@/components/ui/layout/flex"
 import { Heading } from "@/components/ui/typography/heading"
@@ -7,7 +8,7 @@ export function HeaderContent() {
   return (
     <Flex align="center" justify="between" gap="sm">
       <Flex align="center" gap="sm">
-        <ChristmasTree />
+        {/* <ChristmasTree /> */}
         <Flex direction="col">
           <Heading as="h1" weight="semibold" className="text-sm md:text-base">
             Daniel Gaievskyi
